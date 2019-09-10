6ae2cd1dfd4ce56a8e62aa81bf8d4ec800a748fc
Gosuke Miyashita <gosukenator@gmail.com>
2019-09-10 23:36:52 +0900

    Add a route

--- vpc.tf
+++ vpc.tf
@@ -22,3 +22,9 @@ resource "aws_internet_gateway" "example" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.example.id
 }
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
