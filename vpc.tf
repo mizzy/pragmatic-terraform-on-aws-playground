a6f2c06fcd5c13766b1dc6a249542124f47c3005
Gosuke Miyashita <gosukenator@gmail.com>
2019-09-11 08:38:28 +0900

    Multi AZ private subnet

--- vpc.tf
+++ vpc.tf
@@ -46,35 +46,68 @@ resource "aws_route_table_association" "public_1" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_0" {
   vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.64.0/24"
+  cidr_block              = "10.0.65.0/24"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = false
 }
 
-resource "aws_route_table" "private" {
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.66.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private_0" {
   vpc_id = aws_vpc.example.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
 }
 
-resource "aws_eip" "nat_gateway" {
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_eip" "nat_gateway_0" {
   vpc        = true
   depends_on = [aws_internet_gateway.example]
 }
 
-resource "aws_nat_gateway" "example" {
-  allocation_id = aws_eip.nat_gateway.id
+resource "aws_eip" "nat_gateway_1" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "example_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
   subnet_id     = aws_subnet.public_0.id
   depends_on    = [aws_internet_gateway.example]
 }
 
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private.id
-  nat_gateway_id         = aws_nat_gateway.example.id
+resource "aws_nat_gateway" "example_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.example_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.example_1.id
   destination_cidr_block = "0.0.0.0/0"
 }
