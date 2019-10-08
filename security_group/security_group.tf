399d5c299e3290087e71c4ec4976ce6dd40d1678
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-08 19:07:16 +0900

    Fix security group module

--- security_group/security_group.tf
+++ security_group/security_group.tf
@@ -17,14 +17,14 @@ resource "aws_security_group_rule" "ingress" {
   to_port           = var.port
   protocol          = "tcp"
   cidr_blocks       = var.cidr_blocks
-  security_group_id = aws_secutiry_group.default.id
+  security_group_id = aws_security_group.default.id
 }
 
 resource "aws_security_group_rule" "egress" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
-  ptorocol          = "-1"
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.default.id
 }
