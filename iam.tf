e7c7aaae817603b00af9337de572a608bebf6f56
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-08 18:01:17 +0900

    5.2 ロール

--- iam.tf
+++ iam.tf
@@ -11,3 +11,24 @@ resource "aws_iam_policy" "example" {
   policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "example"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example" {
+  role       = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.example.arn
+}
+
