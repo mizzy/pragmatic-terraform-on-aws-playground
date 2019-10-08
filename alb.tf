a8e7a7aea8c37b43a6e8e5324b0c1edef67b2fc7
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-08 20:41:16 +0900

    8.2.2 リスナー

--- alb.tf
+++ alb.tf
@@ -49,3 +49,19 @@ resource "aws_lb" "example" {
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは『HTTP』です"
+      status_code  = "200"
+    }
+  }
+}
