2ab656183693c81e69cb8f1dba4bd3bc723f774b
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-08 17:56:54 +0900

    5.1 ポリシー

---
+++ iam.tf
@@ -0,0 +1,13 @@
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "example" {
+  name   = "example"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
