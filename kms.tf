6c17e4a550cdfc3cc77cb7363c33513068384ef9
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-13 02:25:08 +0900

    11.1.1 カスタマーマスターキー

---
+++ kms.tf
@@ -0,0 +1,7 @@
+resource "aws_kms_key" "example" {
+  description             = "Example Customer Muster Key"
+  enable_key_rotation     = true
+  is_enabled              = true
+  deletion_window_in_days = 30
+}
+
