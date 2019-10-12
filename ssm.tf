3b3ca10bf0ec1fb197b4ecb3b72b15a5f9f8adda
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-13 02:39:15 +0900

    12.2.2 Terraformによるコード化

---
+++ ssm.tf
@@ -0,0 +1,24 @@
+resource "aws_ssm_parameter" "db_username" {
+  name        = "/db/username"
+  value       = "root"
+  type        = "String"
+  description = "データベースの接続ユーザ名"
+}
+
+resource "aws_ssm_parameter" "db_raw_password" {
+  name        = "/db/raw_password"
+  value       = "VeryStrongPassword!"
+  type        = "SecureString"
+  description = "データベースのパスワード"
+}
+
+resource "aws_ssm_parameter" "db_password" {
+  name        = "/db/password"
+  value       = "uninitialized"
+  type        = "SecureString"
+  description = "データベースのパスワード"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
