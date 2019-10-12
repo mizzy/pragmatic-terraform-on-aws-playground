171c5b614346a15ca3e2be0fa34fdec5ee299438
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-12 20:34:22 +0900

    9.2.2 タスク定義

--- ecs.tf
+++ ecs.tf
@@ -2,3 +2,13 @@ resource "aws_ecs_cluster" "example" {
   name = "example"
 }
 
+resource "aws_ecs_task_definition" "example" {
+  family                   = "example"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./container_definitions.json")
+}
+
+
