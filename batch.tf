940030b50c8c666b4e41fbd392220bd3665c240f
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-12 21:24:29 +0900

    10.2.2 CloudWtachイベントIAMロール

--- batch.tf
+++ batch.tf
@@ -12,3 +12,14 @@ resource "aws_ecs_task_definition" "example_batch" {
   container_definitions    = file("./batch_container_definitions.json")
   execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
 }
+
+module "ecs_events_role" {
+  source     = "./iam_role"
+  name       = "ecs-events"
+  identifier = "events.amazonaws.com"
+  policy     = data.aws_iam_policy.ecs_events_role_policy.policy
+}
+
+data "aws_iam_policy" "ecs_events_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
