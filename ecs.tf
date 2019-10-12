2c3fc1af184265db5c555818dc8c375fdeee8ced
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-12 20:56:29 +0900

    9.3.2 ECSタスク実行IAMロール

--- ecs.tf
+++ ecs.tf
@@ -48,3 +48,26 @@ module "nginx_sg" {
   port        = 80
   cidr_blocks = [aws_vpc.example.cidr_block]
 }
+
+
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source     = "./iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
+}
+
