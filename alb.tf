d60d0801144c5fa548a4040b11246c944183ed1a
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-12 16:07:42 +0900

    8.5.2 HTTPのリダイレクト

--- alb.tf
+++ alb.tf
@@ -83,3 +83,19 @@ resource "aws_lb_listener" "https" {
     }
   }
 }
+
+resource "aws_lb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
