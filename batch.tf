29332a1bc926a2d9207e66a10399e7214734b7dd
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-12 21:10:20 +0900

    10.2.1 バッチ用タスク定義

---
+++ batch.tf
@@ -0,0 +1,14 @@
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name              = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
+
+resource "aws_ecs_task_definition" "example_batch" {
+  family                   = "example-batch"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./batch_container_definitions.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+}
