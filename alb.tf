e7f5601a5e62f3bf097940a98a78f06a8126f034
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-12 16:23:36 +0900

    8.6.2 リスナールール

--- alb.tf
+++ alb.tf
@@ -121,3 +121,18 @@ resource "aws_lb_target_group" "example" {
 
   depends_on = [aws_lb.example]
 }
+
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["/*"]
+  }
+}
