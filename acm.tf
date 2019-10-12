14d8c755b113529e7a7e3a39795b64b5122ea343
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-12 14:32:37 +0900

    8.4.2 SSL証明書の検証

--- acm.tf
+++ acm.tf
@@ -7,3 +7,16 @@ resource "aws_acm_certificate" "example" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "example_certificate" {
+  name    = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+  zone_id = data.aws_route53_zone.example.id
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
+}
