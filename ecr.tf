9891b1240f17f8c6f1fedb3b802278ef6126b952
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-14 00:57:12 +0900

    14.2.2 ECRライフサイクルポリシー

--- ecr.tf
+++ ecr.tf
@@ -2,3 +2,7 @@ resource "aws_ecr_repository" "example" {
   name = "example"
 }
 
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.example.name
+  policy     = file("./ecr_lifecycle_policy.json")
+}
