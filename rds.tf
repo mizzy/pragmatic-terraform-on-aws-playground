6b3d8f066dbea69c698acf7389348e88d42aa788
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-13 21:20:43 +0900

    13.1.4 DBインスタンス

--- rds.tf
+++ rds.tf
@@ -27,3 +27,42 @@ resource "aws_db_subnet_group" "example" {
   name       = "example"
   subnet_ids = [aws_subnet.private_0.id, aws_subnet.private_1.id]
 }
+
+resource "aws_db_instance" "example" {
+  identifier                 = "example"
+  engine                     = "mysql"
+  engine_version             = "5.7.23"
+  instance_class             = "db.t3.small"
+  allocated_storage          = 20
+  storage_type               = "gp2"
+  storage_encrypted          = true
+  kms_key_id                 = aws_kms_key.example.arn
+  username                   = "admin"
+  password                   = "VeryStrongPassword"
+  multi_az                   = true
+  publicly_accessible        = false
+  backup_window              = "09:10-09:40"
+  backup_retention_period    = 30
+  maintenance_window         = "mon:10:10-mon:10:40"
+  auto_minor_version_upgrade = false
+  deletion_protection        = true
+  skip_final_snapshot        = false
+  port                       = 3306
+  apply_immediately          = false
+  vpc_security_group_ids     = [module.mysql_sg.security_group_id]
+  parameter_group_name       = aws_db_parameter_group.example.name
+  option_group_name          = aws_db_option_group.example.name
+  db_subnet_group_name       = aws_db_subnet_group.example.name
+
+  lifecycle {
+    ignore_changes = [password]
+  }
+}
+
+module "mysql_sg" {
+  source      = "./security_group"
+  name        = "mysql-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 3306
+  cidr_blocks = [aws_vpc.example.cidr_block]
+}
