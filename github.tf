ab8940f5c9850f1d5ecfec8548abce3bd12fb9e9
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-14 18:23:09 +0900

    14.4.7 GitHub Webhook

--- github.tf
+++ github.tf
@@ -1,3 +1,16 @@
 provider "github" {
   organization = "your-github-name"
 }
+
+resource "github_repository_webhook" "example" {
+  repository = "your-repository"
+
+  configuration {
+    url          = aws_codepipeline_webhook.example.url
+    secret       = "VeryRnadomStringMoreThan20Byte!"
+    content_type = "json"
+    insecure_ssl = false
+  }
+
+  events = ["push"]
+}
