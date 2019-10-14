b84d1c311f964a02d205425c1a921afb869f9aed
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-14 18:17:53 +0900

    14.4.5 CodePipeline Webhook

--- codepipeline.tf
+++ codepipeline.tf
@@ -106,3 +106,19 @@ resource "aws_codepipeline" "example" {
     type     = "S3"
   }
 }
+
+resource "aws_codepipeline_webhook" "example" {
+  name            = "example"
+  target_pipeline = aws_codepipeline.example.name
+  target_action   = "Source"
+  authentication  = "GITHUB_HMAC"
+
+  authentication_configuration {
+    secret_token = "VeryRandomStringMoreThan20Byte!"
+  }
+
+  filter {
+    json_path    = "$.ref"
+    match_equals = "refs/heads/{Branch}"
+  }
+}
