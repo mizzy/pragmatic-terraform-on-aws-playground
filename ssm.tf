cf5dd3ab329e0402d33e36846eed7e3e8e064c55
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-14 18:40:23 +0900

    15.2.1 インスタンスプロファイル

--- ssm.tf
+++ ssm.tf
@@ -22,3 +22,39 @@ resource "aws_ssm_parameter" "db_password" {
     ignore_changes = [value]
   }
 }
+
+data "aws_iam_policy_document" "ec2_for_ssm" {
+  source_json = data.aws_iam_policy.ec2_for_ssm.policy
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BachCheckLayerAvailability",
+      "ecr:GetDownloadUriForLayer",
+      "ecr:BatchGetImage",
+      "sam:GetParameter",
+      "sam:GetParameters",
+      "sam:GetParameterByPath",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+data "aws_iam_policy" "ec2_for_ssm" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+module "ec2_for_ssm_role" {
+  source     = "./iam_role"
+  name       = "ec2-for-ssm"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ec2_for_ssm.json
+}
+
+resource "aws_iam_instance_profile" "ec2_for_sam" {
+  name = "ec2-for-ssm"
+  role = module.ec2_for_ssm_role.iam_role_name
+}
