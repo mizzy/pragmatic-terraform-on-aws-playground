1ce0995c1d65346aaffd7616aefd7f843970ce72
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-15 18:01:30 +0900

    16.3.1 ログ永続化S3バケット

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,3 +2,15 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   name              = "/ecs/example"
   retention_in_days = 180
 }
+
+resource "aws_s3_bucket" "cloudwatch_logs" {
+  bucket = "cloudwatch-logs-pragmatic-terraform-on-aws-mizzy"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
