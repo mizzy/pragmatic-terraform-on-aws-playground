a4bdae75d6af49e8c4fe831f4a3606b1a4904f9a
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-15 18:42:11 +0900

    16.3.4 CloudWatch Logs IAMロール

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -14,3 +14,24 @@ resource "aws_s3_bucket" "cloudwatch_logs" {
     }
   }
 }
+
+data "aws_iam_policy_document" "cloudwatch_logs" {
+  statement {
+    effect    = "Allow"
+    actions   = ["firehose:*"]
+    resources = ["arn:aws:firehose:ap-northeast-1:*:*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
+    resources = ["arn:aws:iam::*:role/cloudwatch-logs"]
+  }
+}
+
+module "cloudwatch_logs_role" {
+  source     = "./iam_role"
+  name       = "cloudwatch-logs"
+  identifier = "logs.ap-northeast-1.amazonaws.com"
+  policy     = data.aws_iam_policy_document.cloudwatch_logs.json
+}
