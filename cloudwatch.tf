65266eaa04a95bb4aa796f619199eafc1dd4553d
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-15 18:57:22 +0900

    1.3.5 CloudAtach Logsサブスクリプションフィルタ

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -35,3 +35,11 @@ module "cloudwatch_logs_role" {
   identifier = "logs.ap-northeast-1.amazonaws.com"
   policy     = data.aws_iam_policy_document.cloudwatch_logs.json
 }
+
+resource "aws_cloudwatch_log_subscription_filter" "example" {
+  name            = "example"
+  log_group_name  = aws_cloudwatch_log_group.for_ecs_scheduled_tasks.name
+  destination_arn = aws_kinesis_firehose_delivery_stream.example.arn
+  filter_pattern  = "[]"
+  role_arn        = module.cloudwatch_logs_role.iam_role_arn
+}
